0a912e24c42a9347799dc9ad68c7a6874e4bf059
es.kim <es.kim@weeds.co.kr>
2025-06-09 11:01:40 +0900

    JAVASCRIPT 생성 및 분기

---
+++ js/custom.js
@@ -0,0 +1,72 @@
+const videoWrapper = document.querySelector('.video-wrapper');
+
+  window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+
+    // 마스크 크기 확대
+    const baseSize = 95.6667;
+    const maxSize = 4000;
+    const newSize = Math.min(baseSize + scrollY * 2, maxSize);
+    videoWrapper.style.webkitMaskSize = `${newSize}vw`;
+    videoWrapper.style.maskSize = `${newSize}vw`;
+
+    // 마스크 위치 이동 (Y값 20% → 55%)
+    const startScroll = 0; // 마스크 위치 변경 시작 스크롤값
+    const endScroll = 500;   // 마스크 위치 변경 완료 스크롤값
+    const startY = 20;
+    const endY = 53;
+
+    let newY = startY;
+    if (scrollY > startScroll) {
+      const progress = Math.min((scrollY - startScroll) / (endScroll - startScroll), 1);
+      newY = startY + (endY - startY) * progress;
+    }
+
+    videoWrapper.style.webkitMaskPosition = `48.1% ${newY}%`;
+    videoWrapper.style.maskPosition = `48.1% ${newY}%`;
+  });
+
+  // Lenis 부드러운 스크롤
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smooth: true,
+    smoothTouch: false,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+	/*
+  // SCROLL MASK-SIZE 확대
+  const videoWrapper = document.querySelector('.video-wrapper');
+
+  window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const baseSize = 80.6667;
+    const maxSize = 1920;
+    const newSize = Math.min(baseSize + scrollY * 2, maxSize);
+    
+    videoWrapper.style.webkitMaskSize = `${newSize}vw`;
+    videoWrapper.style.maskSize = `${newSize}vw`;
+  });
+
+  // Lenis 부드러운 스크롤
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smooth: true,
+    smoothTouch: false,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+  */
